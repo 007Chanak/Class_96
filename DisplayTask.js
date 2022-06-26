1d2112ed34da10b81835817497cc1bc8dca8d176
007Chanak <70619452+007Chanak@users.noreply.github.com>
2022-06-26 19:50:56 +0530

    Add files via upload

---
+++ DisplayTask.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import {CreateSwitchNavigator, createAppContainer} from "react-navigation";
+import {View, TouchableOpacity, StyleSheet} from "react-native";
+import {Card} from "react-native-paper";
+
+export default class DisplayTask extends Components{
+    render(){
+        
+    }
+}
